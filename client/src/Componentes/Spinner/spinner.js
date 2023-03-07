bfcf3287f682d653fd597b5f99ac1e6bf13673ca
daviddflix <david-972010@hotmail.com>
2023-03-07 03:53:51 -0300

    details styles

--- client/src/Componentes/Spinner/spinner.js
+++ client/src/Componentes/Spinner/spinner.js
@@ -1,16 +1,25 @@
-import { useSelector } from 'react-redux'
-import s from './spinner.module.css'
+import { useSelector } from "react-redux";
+import s from "./spinner.module.css";
 
-export default function Spinner (){
+export default function Spinner() {
+  const notFound = useSelector((state) => state.notFound);
 
-    const notFound = useSelector(state => state.notFound)
-    return(
-     <h1>
-         
-              {
-                notFound === false &&   <p className={s.loader}></p>
-            }
-         
-     </h1>
-    )
+  return (
+    <div className={s.container}>
+      <h1>
+        {notFound === false && (
+          <div className={s.ldsRoller}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        )}
+      </h1>
+    </div>
+  );
 }
--- client/src/Componentes/home/home.jsx
+++ client/src/Componentes/home/home.jsx
@@ -1,87 +1,76 @@
-import { useState } from "react"
-import {useDispatch, useSelector} from 'react-redux'
-import { filterAz } from "../../redux/actions"
-import {NavLink} from 'react-router-dom'
-import s from './home.module.css'
-import { Nav } from "../barra/barra"
-import { Paginado } from "../paginado/paginado"
-import { Filtros } from "../filtros/Filtros"
-import Spinner from '../Spinner/spinner.js'
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { filterAz } from "../../redux/actions";
+import { NavLink } from "react-router-dom";
+import s from "./home.module.css";
+import { Nav } from "../barra/barra";
+import { Paginado } from "../paginado/paginado";
+import { Filtros } from "../filtros/Filtros";
+import Spinner from "../Spinner/spinner.js";
 
+export default function Home() {
+  const paises = useSelector((state) => state.paises);
+  const notFound = useSelector((state) => state.notFound);
 
+  const dispatch = useDispatch();
 
-export default function Home(){
-    const paises = useSelector(state => state.paises)
-    const notFound = useSelector(state => state.notFound)
-  
-   const dispatch = useDispatch()
+  let [, setOrder] = useState("");
+  let [currentPage, setcurrentPage] = useState(1);
+  let [countriesPerPage] = useState(8);
 
-  
-   
-     let [, setOrder] = useState('')
-     let [currentPage, setcurrentPage] = useState(1);
-     let [countriesPerPage] = useState(8);
-    
-    
-     const indexOfLastCountry = currentPage * countriesPerPage; // i10
-     const indexOfFirstCountry = indexOfLastCountry - countriesPerPage // 10 - 10 = 0 
-     const currentCountries = paises.slice(indexOfFirstCountry, indexOfLastCountry)
-                     
-     const paginado = (pageNumber) => {
-       setcurrentPage(pageNumber)
-     }
+  const indexOfLastCountry = currentPage * countriesPerPage; // i10
+  const indexOfFirstCountry = indexOfLastCountry - countriesPerPage; // 10 - 10 = 0
+  const currentCountries = paises.slice(
+    indexOfFirstCountry,
+    indexOfLastCountry
+  );
 
+  const paginado = (pageNumber) => {
+    setcurrentPage(pageNumber);
+  };
 
-     
+  const handleAz = (e) => {
+    e.preventDefault();
+    dispatch(filterAz(e.target.value));
+    setcurrentPage(1);
+    setOrder(`${e.target.value}`);
+  };
 
+  // useEffect(()=>{
+  //   if(!paises.length && notFound === false){
+  //     dispatch(getAll())
+  //   }
+  // })
 
-const handleAz = (e)=> {
-  e.preventDefault();
-  dispatch(filterAz(e.target.value));
-  setcurrentPage(1)
-  setOrder(`${e.target.value}`);
+  return (
+    <div className={s.mainContainer}>
+      <Nav />
+      <Filtros handleAz={handleAz} />
+      <Paginado
+        currentPage={currentPage}
+        paginado={paginado}
+        countriesPerPage={countriesPerPage}
+        countries={paises.length}
+      />
 
-}
-
-
-
-// useEffect(()=>{
-//   if(!paises.length && notFound === false){
-//     dispatch(getAll())
-//   }
-// })
-
-
-
-     return(
-      <div className={s.mainContainer}>
-      <Nav/>
-     <Filtros handleAz={handleAz}/>
-      <Paginado currentPage={currentPage} paginado={paginado} countriesPerPage={countriesPerPage} countries={paises.length} />
-     
-      {
-        notFound && <h2 className= {s.notfound}>Sorry Country Not Found</h2>
-      }
-    <div className={s.containerFlags}>
- 
-      {
-        currentCountries.length?
-        currentCountries.map(el => {
-          return(
-            <div key={el.cca3} className={s.container}>
-          <NavLink className={s.title} to={`/detalle/${el.id}`}>
-            <h1>{el.name}</h1>
-            <img className={s.imagen} src={el.flags} alt="Not Found" />
-            <h1>{el.continente}</h1>
-            </NavLink> 
-          </div>
-          )
-        
-        }): <Spinner/>
-      }
-     </div>
-
-
+      {notFound && <h2 className={s.notfound}>The country you are looking for that not exit</h2>}
+      <div className={s.containerFlags}>
+        {currentCountries.length ? (
+          currentCountries.map((el) => {
+            return (
+              <div key={el.cca3} className={s.container}>
+                <NavLink className={s.subContainer} to={`/detalle/${el.id}`}>
+                  <h1 className={s.title}>{el.name}</h1>
+                  <img className={s.imagen} src={el.flags} alt="Flag" />
+                  <h1 className={s.title}>{el.continente}</h1>
+                </NavLink>
+              </div>
+            );
+          })
+        ) : (
+          <Spinner />
+        )}
       </div>
-     )
+    </div>
+  );
 }
